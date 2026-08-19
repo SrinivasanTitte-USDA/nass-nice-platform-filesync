f3bbb9756beb908f4594efa3363490812d89d98b
SrinivasanTitte-USDA <130396119+SrinivasanTitte-USDA@users.noreply.github.com>
2026-08-19 01:21:18 -0400

    add token controller

---
+++ NICE.Platform.FileSync.Api/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NICE.Platform.FileSync.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class TokenController : ControllerBase
+{
+    public TokenController()
+    {
+
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetToken()
+    {
+        // Generate a token (this is just a placeholder, implement your own logic)
+        var token = Guid.NewGuid().ToString();
+        return Ok(new { Token = token });
+    }
+}
